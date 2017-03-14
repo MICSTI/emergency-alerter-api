4beb791b3eb005bf5fc72db745405fd94231aed9
MICSTI <stifter.michael@gmx.net>
2017-03-14 12:58:25 +0100

    Add access control headers to prevent CORS errors

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// allow requests from all resources (prevent CORS errors)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // API routes ================================================
 app.use('/api/nearest/police', require('./controllers/api-police-stations'));
 
